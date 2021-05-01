74bddb35d053bbfd32816b737f8dac56bf4cbc9b
doob187 <60312740+doob187@users.noreply.github.com>
2021-05-01 11:42:38 +0200

    Update wgetfile.sh

--- wgetfile.sh
+++ wgetfile.sh
@@ -15,11 +15,12 @@
 
 sudo $(command -v apt) update -yqq && sudo $(command -v apt) upgrade -yqq
 if [[ ! -x $(command -v git) ]];then sudo $(command -v apt) install git -yqq;fi
-if [[ -d "/opt/dockserver/installer" ]];then
+if [[ ! -d "/opt/dockserver/" ]] ;then 
+if [[ -d "/opt/dockserver/installer" ]];then  sudo $(command -v mkdir) -p /opt/dockserver/;fi
     sudo $(command -v rm) -rf /opt/dockserver/installer 
     sudo git clone --quiet https://github.com/dockserver/installer.git /opt/dockserver/installer
 fi
-if [[ ! -d "/opt/dockserver/installer" ]];then
+if [[ ! -d "/opt/dockserver/installer" ]]; then
     sudo git clone --quiet https://github.com/dockserver/installer.git /opt/dockserver/installer
 fi
 cd /opt/dockserver/installer && $(command -v bash) install.sh
