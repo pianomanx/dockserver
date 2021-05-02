613ad2f94ebf98ff2505d1c9dceba579d73c2485
Aelfa <60222501+aelfa@users.noreply.github.com>
2021-05-02 16:31:40 +0500

    Revert!

--- wgetfile.sh
+++ wgetfile.sh
@@ -19,7 +19,6 @@ done
 if [[ -f "/bin/dockserver" ]];then $(command -v rm) -rf /bin/dockserver;fi
 if [[ ! -f "/bin/dockserver" ]];then
 cat <<'EOF' > /bin/dockserver
-#!/usr/bin/with-contenv bash
 ####################################
 # All rights reserved.              #
 # started from Zero                 #
